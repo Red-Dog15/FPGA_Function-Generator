0d1114a18bdcfcc2ecbb7c16986f1b8d765b683c
Red-Dog15 <nicholascweng@gmail.com>
2025-11-07 13:14:53 +0000

    added dev branch and starting code for loop converter

--- waveLUT.vhd
+++ waveLUT.vhd
@@ -11,8 +11,22 @@ end waveLUT;
 
 architecture Behavioral of waveLUT is
 
-	type wave_array is array (0 to 7) of INTEGER;
+	type wave_array is array (0 to 32) of INTEGER;
 	constant wave	:	wave_array	:=	(0,1,2,1,0,-1,-2,-1);
+	-- create 32 decimal arraty of full wave values
+	constant wave_dec : wave_array := (128,   153,   178,   200,   
+	220,   236,   247,   254,   255,   251,   242,   228,   211,   189,   
+	166,   140,   115,   89,    66,    44,    27,    13,     4,     0,     
+	1,     8);
+	--convert to std logic
+	type wave_logic_array is array (0 to 32) of STD_LOGIC_VECTOR;
+	SIGNAL wave_bits : wave_logic_array := (0);
+	
+	FOR element IN wave_dec GENERATE
+		wave_bits[0] <= STD_LOGIC_VECTOR(unsigned(element));
+	END GENERATE;
+
+	
 	
 	type state_type is (UPDATE_VALUE, HOLD);
 	signal current_state	:	state_type	:=	UPDATE_VALUE;
