474279fa034373d8a1b7eabc368d2e8bfd6289f9
Red-Dog15 <nicholascweng@gmail.com>
2025-11-25 23:45:02 +0000

    inserted and edited vhdl state machine template for switch output, Need to finish that (set outputs and such) and further construct testbench)

--- Wave_mux.vhd
+++ Wave_mux.vhd
@@ -21,6 +21,12 @@ end Wave_mux;
 		
 architecture Behavioral of Wave_mux is
 
+	-- Build an enumerated type for the state machine
+	type state_type is (s0, s1);
+
+	-- Register to hold the current state
+	signal state   : state_type;
+
 
 begin
 	
@@ -45,6 +51,58 @@ begin
 		 wave_out => sin_wave
 	);
 	
+-- Quartus Prime VHDL Template
+-- Safe State Machine
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity safe_state_machine is
+
+	port(
+		clk		 : in	std_logic;
+		input	 : in	std_logic;
+		reset	 : in	std_logic;
+		output	 : out	std_logic_vector(1 downto 0)
+	);
+
+
+begin
+
+	-- Logic to advance to the next state
+	process (clk, reset)
+	begin
+		if reset = '1' then
+			state <= s0;
+		elsif (rising_edge(clk)) then
+			case state is
+				when s0=>
+					if input = '1' then
+						state <= s1;
+					else
+						state <= s0;
+					end if;
+				when s1=>
+					if input = '1' then
+						state <= s2;
+					else
+						state <= s1;
+					end if;
+
+
+	-- Logic to determine output
+	process (state)
+	begin
+		case state is
+			when s0 =>
+				output <= "00";
+			when s1 =>
+				output <= "01";
+			when s2 =>
+				output <= "10";
+		end case;
+	end process;
+
 
 
 end Behavioral;
