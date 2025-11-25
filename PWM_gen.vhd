8f48032c8821b4e7684acccede4cd4fd4d5fe5f2
Red-Dog15 <nicholascweng@gmail.com>
2025-11-25 20:36:00 +0000

    added script to wave multiplexer w/ instantiation

--- PWM_gen.vhd
+++ PWM_gen.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity PWM_gen is
+
     Port(
         clk        : in std_logic;
         reset      : in std_logic;
--- Wave_mux.vhd
+++ Wave_mux.vhd
@@ -1,1 +1,39 @@
 -- Wave Multiplexer
+library IEEE;
+use IEEE.STD_Logic_1164.ALL;
+Use IEEE.NUMERIC_STD.ALL; 
+
+entity Wave_mux is
+
+	port(
+		clk 	:	in	STD_LOGIC;
+		wave_out	:	out	STD_LOGIC_VECTOR(7 downto 0);
+		
+		reset   :      in	STD_LOGIC;                     -- reset button
+		btn0_in    :     in	STD_LOGIC;                   -- push button input (prefer active-high)
+		btn1_in    :     in	STD_LOGIC                   -- push button input (prefer active-high)
+	);
+	
+end Wave_mux;
+
+		
+architecture Behavioral of Wave_mux is
+
+
+begin
+
+
+PWM0 : entity work.PWM_gen
+port map(
+    clk => clk,
+    reset => reset,
+    duty => 50,   -- TEMP constant
+    pwm_out => pwm_wave(7)
+);
+
+
+
+-- tie remaining bits
+pwm_wave(6 downto 0) <= (others => pwm_wave(7));
+
+end Behavoiral
