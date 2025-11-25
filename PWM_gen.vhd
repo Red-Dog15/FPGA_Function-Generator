d8210574cc2aa033fa95730b9da7c9e7cc36fba6
Red-Dog15 <nicholascweng@gmail.com>
2025-11-25 21:06:34 +0000

    set up PWM_gen, need to make tweaks to sort out issues

--- PWM_gen.vhd
+++ PWM_gen.vhd
@@ -7,7 +7,7 @@ entity PWM_gen is
     Port(
         clk        : in std_logic;
         reset      : in std_logic;
-        duty       : in integer range 0 to 100;   -- duty % 
+        duty       : in integer range 0 to 100;   -- duty % cycle
         pwm_out    : out std_logic;
 		  btn0_in	:  in std_logic;
 		  btn1_in  L:	in std_logic;
@@ -20,10 +20,46 @@ architecture Behavioral of PWM_gen is
     signal cnt : integer range 0 to 99 := 0;
 	 signal plus_pulse    : integer := 0; -- hold incrementer states
     signal minus_pulse   : integer := 0;
-	
+    signal duty_incrementer   : integer := 10;
+
 begin
+
+-- instantiate 2 instance of button incrementer, button plut (positive incrementer) & button minus (negative incrementer)
+	Btn_plus : entity work.Button_Incrementer -- buton plus instationation
+	
+		Generic map (
+        	CLK_FREQ_HZ => 50000000,
+        	DEBOUNCE_MS => 20,
+			btn_val	 =>	  duty_incrementer    -- set  output to duty incrementer singal
+		)
+		port map (
+			clk     =>      clk,                          -- system clock
+			reset    =>     reset,                     -- reset button
+			btn_in    =>     btn0_in,                   -- push button input (prefer active-high), set to button 0
+			output   =>   plus_pulse        -- incrementer state plus
+
+		);
+--			
+	Btn_minus : entity work.Button_Incrementer -- buton minus instationation
+		
+		Generic map (
+        		CLK_FREQ_HZ => 50000000,
+        		DEBOUNCE_MS => 20,
+				btn_val	 =>	  -duty_incrementer    -- set to -dutyincrementer signal for minus pulse
+		)
+		port map (
+		
+			clk     =>      clk,                          -- system clock
+			reset    =>     reset,                     -- reset button
+			btn_in    =>     btn1_in,                   -- set to button 1
+			output   =>   minus_pulse         -- incrementer state minus
+
+		);
+
+			
     process(clk, reset)
     begin
+		  duty = duty + plus_pulse + minus_pulse -- increment duty cycle by pulses
         if reset = '0' then
             cnt <= 0;
             pwm_out <= '0';
