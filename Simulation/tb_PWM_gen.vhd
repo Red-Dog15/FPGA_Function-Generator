2a07b69d520247af124354e83324c721601733b1
Red-Dog15 <nicholascweng@gmail.com>
2025-11-26 14:29:25 +0000

    testing .gitignore

---
+++ Simulation/tb_PWM_gen.vhd
@@ -0,0 +1,82 @@
+--	Test Bench for PWM Generator
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity PWM_gen_tb is
+end PWM_gen_tb;
+
+architecture Behavioral of PWM_gen_tb is
+        signal clk        :  STD_LOGIC	:=	'0';
+        signal reset      : STD_LOGIC := '1';
+        signal pwm_out    : STD_LOGIC_VECTOR(7 DOWNTO 0);
+		  signal btn0_in	:  STD_LOGIC := '1';
+		  signal btn1_in  :	 STD_LOGIC := '1';
+
+begin
+
+    -- instantiate DUT
+    dut : entity work.PWM_gen
+        port map (
+            clk      => clk,
+            pwm_out => pwm_out,
+            reset    => reset,
+            btn0_in  => btn0_in,
+            btn1_in  => btn1_in
+        );
+
+	
+        -- clock generation (20 ns period = 50 MHz)
+    -- CLOCK PROCESS  (runs forever)
+
+    clk_process : process
+    begin
+        clk <= '0';
+        wait for 10 ns;
+        clk <= '1';
+        wait for 10 ns;
+    end process;
+
+
+
+    -- STIMULUS
+
+    stim_proc : process
+    begin
+        
+        -- global reset pulse
+        reset <= '0';
+        wait for 100 ns;
+        reset <= '1';
+        wait for 200 ns;
+        
+        
+
+        -- let waveform run freely for a bit
+        wait for 3 us;
+        
+
+        -- press BTN0 once (increase speed)
+        btn0_in <= '0';
+        wait for 21 ms; -- wait longer than 20 to activate debouncer
+        btn0_in <= '1';
+        wait for 200 ns;
+	btn0_in <= '0';
+        wait for 21 ms;
+        btn0_in <= '1';
+        wait for 200 ns;
+
+        -- press BTN1 once (decrease speed)
+        btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;
+    	btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;       
+	wait;
+        
+    end process;
+end Behavioral;
---
+++ Simulation/tb_Wave_mux.vhd
@@ -0,0 +1,158 @@
+-- Testbench for Wave Multiplexer (Wave_mux)
+
+--	Test Bench for Wave_mux
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Wave_mux_tb is
+end Wave_mux_tb;
+
+architecture Behavioral of Wave_mux_tb is
+        signal clk        :  STD_LOGIC	:=	'0';
+        signal reset      : STD_LOGIC := '1';
+		  signal btn0_in	:  STD_LOGIC := '1';
+		  signal btn1_in  :	 STD_LOGIC := '1';
+		  signal sw0, sw1 :  	STD_LOGIC; 
+		  signal wave_out	:		STD_LOGIC_VECTOR(7 downto 0);
+		  signal LEDs	:	 std_logic_vector(2 downto 0);	
+		  signal SSEG0, SSEG1 :  std_logic_vector(7 downto 0);
+		  -- state LEDS
+ 
+begin
+
+    -- instantiate DUT
+    dut : entity work.Wave_mux
+        port map (
+            clk      => clk,
+            wave_out => wave_out,
+            reset    => reset,
+            btn0_in  => btn0_in,
+            btn1_in  => btn1_in,
+				LEDs	=> LEDs,
+				sw0 => sw0,
+				sw1 => sw1,
+				SSEG0	=> SSEG0,
+				SSEG1	=> SSEG1
+        );
+
+	
+        -- clock generation (20 ns period = 50 MHz)
+    -- CLOCK PROCESS  (runs forever)
+
+    clk_process : process
+    begin
+        clk <= '0';
+        wait for 10 ns;
+        clk <= '1';
+        wait for 10 ns;
+    end process;
+
+
+
+    -- STIMULUS
+
+    stim_proc : process
+    begin
+        -- Switches = pwm state
+		  sw0 <= '1';
+		  sw1 <= '0';
+		  
+        -- global reset pulse
+        reset <= '0';
+        wait for 100 ns;
+        reset <= '1';
+        wait for 200 ns;
+        
+        
+
+        -- let waveform run freely for a bit
+        wait for 3 us;
+        
+
+        -- press BTN0 once (increase speed)
+        btn0_in <= '0';
+        wait for 21 ms; -- wait longer than 20 to activate debouncer
+        btn0_in <= '1';
+        wait for 200 ns;
+		  btn0_in <= '0';
+        wait for 21 ms;
+        btn0_in <= '1';
+        wait for 200 ns;
+
+        -- press BTN1 once (decrease speed)
+        btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;
+    	  btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;       
+		  
+	    
+        -- global reset pulse
+        reset <= '0';
+        wait for 100 ns;
+        reset <= '1';
+        wait for 200 ns;
+        
+        
+
+        -- let waveform run freely for a bit
+        wait for 3 us;
+		  
+        -- Switches = wave state
+		  sw0 <= '0';
+		  sw1 <= '1';
+
+        -- press BTN0 once (increase speed)
+        btn0_in <= '0';
+        wait for 21 ms; -- wait longer than 20 to activate debouncer
+        btn0_in <= '1';
+        wait for 200 ns;
+		  btn0_in <= '0';
+        wait for 21 ms;
+        btn0_in <= '1';
+        wait for 200 ns;
+
+        -- press BTN1 once (decrease speed)
+        btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;
+    	  btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;   
+
+		  
+        -- Switches = null state
+		  sw0 <= '1';
+		  sw1 <= '1';
+
+        -- press BTN0 once (increase speed)
+        btn0_in <= '0';
+        wait for 21 ms; -- wait longer than 20 to activate debouncer
+        btn0_in <= '1';
+        wait for 200 ns;
+		  btn0_in <= '0';
+        wait for 21 ms;
+        btn0_in <= '1';
+        wait for 200 ns;
+
+        -- press BTN1 once (decrease speed)
+        btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;
+    	  btn1_in <= '0';
+        wait for 21 ms;
+        btn1_in <= '1';
+        wait for 200 ns;    
+		  
+	wait;
+        
+    end process;
+end Behavioral;
